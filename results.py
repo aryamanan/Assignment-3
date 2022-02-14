e9bb26cead970c60623128fbc8b15caaae9122e4
Aryan Arora <aryanarora190402@gmail.com>
2022-02-14 23:44:03 +0530

    Update results.py

--- results.py
+++ results.py
@@ -1,9 +1,92 @@
-def q6():
-    sides=[]
-    for i in range(3):
-        side=float(input("Enter side %s: "%(i+1)))
-        sides.append(side)
-    hello=input("what u want")
+Give a string-hahahahihi
+{'H': 5, 'A': 3, 'I': 2}
+Give month-3
+Give day-12
+Give year-2015
+Date- 12 / 3 / 2015
+Next Date- 13 / 3 / 2015
+Done!
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Done!
+Give a number between 4 and 10 including them-5
+Performance=Below Average
+Letter Grade=C
+Done!
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+Done!
+----------------(a)-------------------
+Give the sid (a number):5
+Enter your name:aryan
+give a letter y or n:n
+The required dictionary-- {5: 'aryan'}
+The list will be used in further parts--
+The required list-- [(5, 'aryan')]
+Done!
+----------------(b)------------------------
+The dictionary we had-
+{5: 'aryan'}
+now we have exchanged the key value pair but its not sorted-
+{'aryan': 5}
+The unsorted list--
+[('aryan', 5)]
+The sorted list--
+[('aryan', 5)]
+The sorted dictionary-
+{'aryan': 5}
+This is the dictionary sorted on the basis of name-
+{5: 'aryan'}
+Done!
+-----------------(c)-----------------
+The list we made before will be used now-
+[(5, 'aryan')]
+now the list is sorted-
+[(5, 'aryan')]
+sorted dictionary based on sid- {5: 'aryan'}
+Done!
+-----------------(d)-----------------
+Give a number-1
+Give a number-2
+[1, 2, 3]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+Give y to contnue and n to stop going further-n
+Now we got a list having a fibonacci series-
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+Average of the sequence-
+34.09090909090909
+Done!
 
-q6()
-
+-------------(a)------------
+{1, 3, 5, 6, 8}
+Done!
+-------------(b)------------
+{3, 6, 8, 9, 13, 17}
+Done!
+-------------(c)------------
+{1, 2, 4, 5}
+Done!
+-------------(d)------------
+{6, 7, 8, 9, 10}
+Done!
+-------------(e)------------
+{10, 7}
+Done!
